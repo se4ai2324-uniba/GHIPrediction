7487537b72ca396bec0a6d5a6a180537c20b6e4d
Fra3005 <francescodedeoo@gmail.com>
2023-10-24 16:58:17 +0200

    upload make_dataset file

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,22 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+class Dataset:
+  
+#Create CSV
+    def finalDf(self, df, df1, df2, name):
+        data = [df, df1, df2]
+        final = pd.DataFrame()
+        final = pd.concat(data)
+        final.to_csv(f"data/raw/{name}", index = False)
+        df = pd.read_csv(f"data/raw/{name}", delimiter = ',')
+        return df
+    
+
+dataset = Dataset()
+dfB1 = pd.read_csv('data/raw/2017.csv', delimiter = ',')
+dfB2 = pd.read_csv('data/raw/2018.csv', delimiter = ',')
+dfB3 = pd.read_csv('data/raw/2019.csv', delimiter = ',')
+dfBari = dataset.finalDf(dfB1, dfB2, dfB3, 'FinaleBari.csv')
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
 
-
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    
