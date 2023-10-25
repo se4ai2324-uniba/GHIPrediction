96c0c85a7d1a678caed3182f4d260e6e5d5da944
Fra3005 <francescodedeoo@gmail.com>
2023-10-25 11:41:12 +0200

    changes in dependecies yaml

--- src/models/RandomForestRegressor.py
+++ src/models/RandomForestRegressor.py
@@ -4,7 +4,7 @@ from train_model import bestHyper
 from train_model import x_train, y_train, x_test, y_test
 import pickle
 from train_model import stampa, predictAndResults
-
+import pandas as pd
 class RandomForest:
 
     def trainRandomForest(self):
@@ -13,6 +13,8 @@ class RandomForest:
         param_grid = {
             'max_depth':  [4,5,6],
             'n_estimators' : list(range(100,160, 10))}
+        x_train= pd.read_csv('data/interim/x_train.csv')
+        y_train= pd.read_csv('data/interim/y_train.csv')
         estimator, result = bestHyper(param_grid, x_train, y_train, rf)
         best_randomB = result.best_estimator_
 
@@ -27,6 +29,8 @@ class RandomForest:
         return best
     
     def testModel(self, model):
+        x_test= pd.read_csv('data/interim/x_test.csv')
+        y_test= pd.read_csv('data/interim/y_test.csv')
         risultati = predictAndResults(model, x_test, y_test)
         stampa(risultati, "RandomForest")
 
