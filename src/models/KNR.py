b41d52dfb2b27096e6efe190e95175d05836cea0
GianFederico <gianfederico.poli@gmail.com>
2023-11-05 10:39:53 +0100

    initializing pylint

--- src/models/KNR.py
+++ src/models/KNR.py
@@ -1,38 +1,28 @@
 from sklearn.neighbors import KNeighborsRegressor
-from train_model import bestHyper, gridLog, save_metrics, save_model
-import pickle
-from train_model import stampa, predictAndResults, use_split
+from train_model import bestHyper, gridLog, save_metrics, save_model, stampa, predictAndResults, use_split
 
 
-class KNR:
+class Knr:
 
     def trainKNR(self):
-       
         knr = KNeighborsRegressor()
         param_grid = {
             'n_neighbors': list(range(5,10,1)),
             'metric' : ['euclidean', 'manhattan', 'chebyshev', 'minkowski']
-                    }
-        
+            }
         x_train, y_train, x_test, y_test = use_split('split_train', 'split_test')
         estimator, grid = bestHyper(param_grid, x_train, y_train, knr)
-        result=self.testModel(estimator)
+        result=self.test_model(estimator)
         save_model(estimator, 'knr')
         gridLog("knr", knr, grid, result, x_test, estimator)
         save_metrics(result, 'knr')
-        #best_random = grid.best_estimator_
-
         return result
 
-    def testModel(self, model):
+    def test_model(self, model):
         _, _, x_test, y_test = use_split('split_train', 'split_test')
         risultati = predictAndResults(model, x_test, y_test)
         stampa(risultati, "KNR")
         return risultati
 
-
-knr = KNR()
+knr = Knr()
 knr.trainKNR()
-
-
-  
