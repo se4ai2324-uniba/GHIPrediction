b1c4a8d74ad8f49001010c41a159730d179d5967
GianFederico <gianfederico.poli@gmail.com>
2023-11-06 14:28:12 +0100

    pylint ok for make_dataset and preprocessing

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -1,34 +1,33 @@
 
+"""module for preprocessing the data"""
 import pandas as pd
 from sklearn.preprocessing import RobustScaler
 
-#Clean csv
 class Preprocessing:
-  def preProcessing(self):
-    df = pd.read_csv('data/raw/FinaleBari.csv', header=0) 
-    i = df[((df.GHI == 0))].index
-    df = df.drop(i)
-    df = df.drop(columns = ['DHI','Year','Month','Day','Hour', 'Minute', 'Precipitable Water', 'Dew Point', 'Cloud Type', 'Surface Albedo','Wind Speed','Pressure'])
-    df = df.fillna(0)
-    return df
+    """class representing preprocessing process"""
+    def pre_processing(self):
+        """function to preprocess data frame"""
+        df = pd.read_csv('data/raw/FinaleBari.csv', header=0)
+        i = df[((df.GHI == 0))].index
+        new_df = df.drop(i)
+        new_new_df = new_df.drop(columns = ['DHI','Year','Month','Day','Hour', 'Minute',
+        'Precipitable Water', 'Dew Point', 'Cloud Type', 'Surface Albedo','Wind Speed','Pressure'])
+        current_df = new_new_df.fillna(0)
+        return current_df
 
-#Scalarization
-  def scalarization(self):
-    scaler = RobustScaler()
-    tmp = df.drop('GHI', axis = 1)
-    scaler.fit(tmp)
-    dfTransformed = scaler.transform(tmp)
-    return dfTransformed
-  
-
+    def scalarization(self, current_df):
+        """function to scalarize data frame"""
+        scaler = RobustScaler()
+        tmp = current_df.drop('GHI', axis = 1)
+        scaler.fit(tmp)
+        df_transformed = scaler.transform(tmp)
+        return df_transformed
 
 preprocessing = Preprocessing()
-
-df = preprocessing.preProcessing()
-ghi = df.GHI
-
-dfS = preprocessing.scalarization()
-
+final_df = preprocessing.pre_processing()
+ghi = final_df.GHI
+dfS = preprocessing.scalarization(final_df)
 dfNew = pd.DataFrame(dfS)
-dfNew.to_csv('data/processed/PreprocessedData.csv', header=['Temperature', 'DNI', 'Relative Humidity'])
+dfNew.to_csv('data/processed/PreprocessedData.csv', header=['Temperature', 'DNI',
+'Relative Humidity'])
 ghi.to_csv('data/processed/GHI.csv')
