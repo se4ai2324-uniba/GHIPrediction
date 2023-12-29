a9f7db475c14c8cb0f8eec574e290234aabea336
GianFederico <gianfederico.poli@gmail.com>
2023-12-29 11:35:15 +0100

    balanced tasks in locustfile

--- locustfile.py
+++ locustfile.py
@@ -5,14 +5,14 @@ class TestUser(HttpUser):
 
     wait_time = between(1, 5)
 
-    @task
+    @task(1)
     def get_best_model(self):
         self.client.get('/api/best_model', name = 'Get_Model')
 
-    @task
+    @task(2)
     def get_info(self):
         self.client.get('', name = 'Get_info')
 
-    @task 
+    @task(1)
     def get_prediction(self):
         self.client.post("/api/prediction", json={"temperature":0,"dni":0,"humidity":0}, name = 'Get_Prediction')
